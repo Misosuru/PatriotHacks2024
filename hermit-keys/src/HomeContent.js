ac3a37a064e02e63aaa63552c2e6ea14e46224d9
boingocs <bnguye70@gmu.edu>
2024-10-12 16:54:10 -0400

    Added description

--- hermit-keys/src/HomeContent.js
+++ hermit-keys/src/HomeContent.js
@@ -6,10 +6,22 @@ const HomeContent = () => {
     <div className="home-container">
       <div className="content-container">
         <div className="text-container">
-          <h3>Welcome to HermitKeys</h3>
+          <h3>Welcome to HermitKeys!</h3>
           <p>
-            Find your perfect home with us. Explore various options tailored to
-            your needs!
+            Having trouble singling out your forever home? Looking for that
+            perfect place that feels just right? Look no further!{" "}
+          </p>
+          <p>
+            HermitKeys is the ultimate platform dedicated to helping you
+            discover your ideal living space. We want to help you find your
+            ideal home, transforming that dream vacation retreat into your
+            forever home. We provide quality options tailored to your needs.
+            With our intuitive search tools, you can easily explore a wide range
+            of housing options designed to fit your budget and lifestyle
+            preferences. Just enter your preferred zip code and must-haves, and
+            let us do the rest! We know that the first step to finding a home
+            can be a bit of a doozy, but we’re here to help. Let HermitKeys
+            guide you on your home journey.
           </p>
         </div>
         <div className="image-container">
